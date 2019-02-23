014a7c2adc6e35ed355d41acec0e27b1d88ea2c7
Koushik Naskar <koushik.naskar9@gmail.com>
2019-02-23 18:10:49 +0530

    api change

--- JsCSS/functions.js
+++ JsCSS/functions.js
@@ -28,13 +28,13 @@ function versionCheck() {
     if (today == JSON.parse(localStorage.getItem("today"))) return
     localStorage.setItem("today", JSON.stringify(today))
     req({
-        'url': "https://api.github.com/repos/Koushikphy/Interactive-Data-Editor/tags",
+        'url': "https://api.github.com/repos/Koushikphy/Interactive-Data-Editor/releases/latest",
         'headers': {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36});'
         }
     },
-        function (err, resp, body) {
-            var new_ver = JSON.parse(body)[0].name
+        function (_, _, body) {
+            var new_ver = JSON.parse(body).name
             console.log(new_ver)
             if (new_ver != "v2.3.0") {
                 var txt = `A new version of the software ${new_ver} is available.\n Do you wnat to downlod it now?`
