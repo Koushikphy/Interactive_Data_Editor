108430d2907da52fc5a96733ed66a07a72a16730
Koushik Naskar <koushik.naskar9@gmail.com>
2019-03-14 16:48:56 +0530

    resolve #79 some changes

--- gulpFile.js
+++ gulpFile.js
@@ -1,4 +1,4 @@
-const gulp = require('gulp');
+const {src, dest, parallel, watch} = require('gulp');
 const rename = require('gulp-rename');
 const uglifyes = require('uglify-es');
 const composer = require('gulp-uglify/composer');
@@ -8,30 +8,34 @@ const cleanCSS = require('gulp-clean-css');
 
 
 
-gulp.task('compressCombineJS', function () {
-    return gulp.src(['./src/functions.js','./src/dataOp.js','./src/keyboardOp.js','./src/plotSetup.js'])
+function compressCombineJS() {
+    return src(['./src/functions.js','./src/dataOp.js','./src/keyboardOp.js','./src/plotSetup.js'])
         .pipe(concat('funcs.js'))
         .pipe(uglify())
         .pipe(rename({suffix : '.min'}))
-        .pipe(gulp.dest('./lib'))
-});
+        .pipe(dest('./lib'))
+}
 
-gulp.task('compressJS', function() {
-    return gulp.src(['./src/init.js', './src/viewer.js'])
+function compressJS() {
+    return src(['./src/init.js', './src/viewer.js'])
         .pipe(uglify())
         .pipe(rename({suffix : '.min'}))
-        .pipe(gulp.dest('./lib'))
-})
+        .pipe(dest('./lib'))
+}
 
-gulp.task('compressCSS', function () {
-    return gulp.src('src/style.css')
+function compressCSS() {
+    return src('src/style.css')
         .pipe(cleanCSS())
         .pipe(rename({suffix : '.min'}))
-        .pipe(gulp.dest('./lib'))
-});
+        .pipe(dest('./lib'))
+}
 
-gulp.task('default', gulp.parallel('compressJS', 'compressCSS','compressCombineJS'));
 
-gulp.task('watch', function(){
-    gulp.watch('./src/*.*', gulp.series('default')); 
-})
+
+
+exports.default = parallel(compressJS, compressCSS,compressCombineJS);
+
+exports.watch =function (){
+    watch('./src/*.*', parallel('default')); 
+}
+
