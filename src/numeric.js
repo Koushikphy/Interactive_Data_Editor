520cf05ab753465d13e9c87a6a394adbdd36f2e2
Koushik Naskar <koushik.naskar9@gmail.com>
2019-10-10 20:52:13 +0530

    up

--- src/numeric.js
+++ src/numeric.js
@@ -342,6 +342,275 @@ function transpose(m) {
 
 
 
+function SVD(a, options) {
+    let { u: withu, v: withv, eps } = Object.assign({ u: true, v: true, eps: Math.pow(2, -52) }, options);
+    const tol = 1e-64 / eps;
+    // throw error if a is not defined
+    if (!a) {
+        throw new TypeError("Matrix a is not defined");
+    }
+    // Householder's reduction to bidiagonal form
+    const n = a[0].length;
+    const m = a.length;
+    if (m < n) {
+        throw new TypeError("Invalid matrix: m < n");
+    }
+    let l1, c, f, h, s, y, z;
+    let l = 0, g = 0, x = 0;
+    const e = [];
+    const u = [];
+    const v = [];
+    // Initialize u
+    for (let i = 0; i < m; i++) {
+        u[i] = new Array(n).fill(0);
+    }
+    // Initialize v
+    for (let i = 0; i < n; i++) {
+        v[i] = new Array(n).fill(0);
+    }
+    // Initialize q
+    const q = new Array(n).fill(0);
+    // Copy array a in u
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            u[i][j] = a[i][j];
+        }
+    }
+    for (let i = 0; i < n; i++) {
+        e[i] = g;
+        s = 0;
+        l = i + 1;
+        for (let j = i; j < m; j++) {
+            s += Math.pow(u[j][i], 2);
+        }
+        if (s < tol) {
+            g = 0;
+        }
+        else {
+            f = u[i][i];
+            g = f < 0 ? Math.sqrt(s) : -Math.sqrt(s);
+            h = f * g - s;
+            u[i][i] = f - g;
+            for (let j = l; j < n; j++) {
+                s = 0;
+                for (let k = i; k < m; k++) {
+                    s += u[k][i] * u[k][j];
+                }
+                f = s / h;
+                for (let k = i; k < m; k++) {
+                    u[k][j] = u[k][j] + f * u[k][i];
+                }
+            }
+        }
+        q[i] = g;
+        s = 0;
+        for (let j = l; j < n; j++) {
+            s += Math.pow(u[i][j], 2);
+        }
+        if (s < tol) {
+            g = 0;
+        }
+        else {
+            f = u[i][i + 1];
+            g = f < 0 ? Math.sqrt(s) : -Math.sqrt(s);
+            h = f * g - s;
+            u[i][i + 1] = f - g;
+            for (let j = l; j < n; j++) {
+                e[j] = u[i][j] / h;
+            }
+            for (let j = l; j < m; j++) {
+                s = 0;
+                for (let k = l; k < n; k++) {
+                    s += u[j][k] * u[i][k];
+                }
+                for (let k = l; k < n; k++) {
+                    u[j][k] = u[j][k] + s * e[k];
+                }
+            }
+        }
+        y = Math.abs(q[i]) + Math.abs(e[i]);
+        if (y > x) {
+            x = y;
+        }
+    }
+    // Accumulation of right-hand transformations
+    if (withv) {
+        for (let i = n - 1; i >= 0; i--) {
+            if (g !== 0) {
+                h = u[i][i + 1] * g;
+                for (let j = l; j < n; j++) {
+                    v[j][i] = u[i][j] / h;
+                }
+                for (let j = l; j < n; j++) {
+                    s = 0;
+                    for (let k = l; k < n; k++) {
+                        s += u[i][k] * v[k][j];
+                    }
+                    for (let k = l; k < n; k++) {
+                        v[k][j] = v[k][j] + s * v[k][i];
+                    }
+                }
+            }
+            for (let j = l; j < n; j++) {
+                v[i][j] = 0;
+                v[j][i] = 0;
+            }
+            v[i][i] = 1;
+            g = e[i];
+            l = i;
+        }
+    }
+    // Accumulation of left-hand transformations
+    if (withu) {
+        for (let i = n - 1; i >= 0; i--) {
+            l = i + 1;
+            g = q[i];
+            for (let j = l; j < n; j++) {
+                u[i][j] = 0;
+            }
+            if (g !== 0) {
+                h = u[i][i] * g;
+                for (let j = l; j < n; j++) {
+                    s = 0;
+                    for (let k = l; k < m; k++) {
+                        s += u[k][i] * u[k][j];
+                    }
+                    f = s / h;
+                    for (let k = i; k < m; k++) {
+                        u[k][j] = u[k][j] + f * u[k][i];
+                    }
+                }
+                for (let j = i; j < m; j++) {
+                    u[j][i] = u[j][i] / g;
+                }
+            }
+            else {
+                for (let j = i; j < m; j++) {
+                    u[j][i] = 0;
+                }
+            }
+            u[i][i] = u[i][i] + 1;
+        }
+    }
+    // Diagonalization of the bidiagonal form
+    eps = eps * x;
+    let testConvergence;
+    for (let k = n - 1; k >= 0; k--) {
+        for (let iteration = 0; iteration < 50; iteration++) {
+            // test-f-splitting
+            testConvergence = false;
+            for (l = k; l >= 0; l--) {
+                if (Math.abs(e[l]) <= eps) {
+                    testConvergence = true;
+                    break;
+                }
+                if (Math.abs(q[l - 1]) <= eps) {
+                    break;
+                }
+            }
+            if (!testConvergence) {
+                // cancellation of e[l] if l>0
+                c = 0;
+                s = 1;
+                l1 = l - 1;
+                for (let i = l; i < k + 1; i++) {
+                    f = s * e[i];
+                    e[i] = c * e[i];
+                    if (Math.abs(f) <= eps) {
+                        break; // goto test-f-convergence
+                    }
+                    g = q[i];
+                    q[i] = Math.sqrt(f * f + g * g);
+                    h = q[i];
+                    c = g / h;
+                    s = -f / h;
+                    if (withu) {
+                        for (let j = 0; j < m; j++) {
+                            y = u[j][l1];
+                            z = u[j][i];
+                            u[j][l1] = y * c + z * s;
+                            u[j][i] = -y * s + z * c;
+                        }
+                    }
+                }
+            }
+            // test f convergence
+            z = q[k];
+            if (l === k) {
+                // convergence
+                if (z < 0) {
+                    // q[k] is made non-negative
+                    q[k] = -z;
+                    if (withv) {
+                        for (let j = 0; j < n; j++) {
+                            v[j][k] = -v[j][k];
+                        }
+                    }
+                }
+                break; // break out of iteration loop and move on to next k value
+            }
+            // Shift from bottom 2x2 minor
+            x = q[l];
+            y = q[k - 1];
+            g = e[k - 1];
+            h = e[k];
+            f = ((y - z) * (y + z) + (g - h) * (g + h)) / (2 * h * y);
+            g = Math.sqrt(f * f + 1);
+            f = ((x - z) * (x + z) + h * (y / (f < 0 ? f - g : f + g) - h)) / x;
+            // Next QR transformation
+            c = 1;
+            s = 1;
+            for (let i = l + 1; i < k + 1; i++) {
+                g = e[i];
+                y = q[i];
+                h = s * g;
+                g = c * g;
+                z = Math.sqrt(f * f + h * h);
+                e[i - 1] = z;
+                c = f / z;
+                s = h / z;
+                f = x * c + g * s;
+                g = -x * s + g * c;
+                h = y * s;
+                y = y * c;
+                if (withv) {
+                    for (let j = 0; j < n; j++) {
+                        x = v[j][i - 1];
+                        z = v[j][i];
+                        v[j][i - 1] = x * c + z * s;
+                        v[j][i] = -x * s + z * c;
+                    }
+                }
+                z = Math.sqrt(f * f + h * h);
+                q[i - 1] = z;
+                c = f / z;
+                s = h / z;
+                f = c * g + s * y;
+                x = -s * g + c * y;
+                if (withu) {
+                    for (let j = 0; j < m; j++) {
+                        y = u[j][i - 1];
+                        z = u[j][i];
+                        u[j][i - 1] = y * c + z * s;
+                        u[j][i] = -y * s + z * c;
+                    }
+                }
+            }
+            e[l] = 0;
+            e[k] = f;
+            q[k] = x;
+        }
+    }
+    // Number below eps should be zero
+    for (let i = 0; i < n; i++) {
+        if (q[i] < eps)
+            q[i] = 0;
+    }
+    return { u, q, v };
+}
+
+
+
 
 class Regression{
     constructor(xs,ys,n, mp=false){
@@ -350,9 +619,9 @@ class Regression{
         this.n=n;
         // normalize the x array
         var ss =0;
-        for(let xx of x) ss+=xx;
-        this.me = ss/x.length;
-        for(let i=0; i<x.length;i++) x[i] -= this.me
+        // for(let xx of x) ss+=xx;
+        // this.me = ss/x.length;
+        // for(let i=0; i<x.length;i++) x[i] -= this.me
 
         if ((n==2) & !mp ) { //explicit formula for quadratic
             this.cf = [0,0,0];
@@ -396,11 +665,17 @@ class Regression{
             u = USV.U
             v = USV.V
             s = USV.S
-            sf = new Array(s.length).fill(0).map(_=> new Array(s.length).fill(0))
-            // inverse of eigenvalus, check if non zero
-            for(let i=0; i<s.length;i++) if (s[i]>Number.EPSILON) sf[i][i] = 1/s[i]
-            pse = matmul(matmul(v, sf), transpose(u))
-            this.cf = matvecmul(pse, y)
+            console.log(new Float32Array(10))
+            console.log(SVD(aa))
+            // // sf = new Array(s.length).fill(0).map(_=> new Array(s.length).fill(0))
+            // sf = new Array(aa.length).fill(0).map(_=> new Array(aa[0].length).fill(0))
+            // // inverse of eigenvalus, check if non zero
+            // for(let i=0; i<s.length;i++) if (s[i]>Number.EPSILON) sf[i][i] = 1/s[i]
+            // var mmm = matmul(v, transpose(sf))
+            // console.log(u)
+            // pse = matmul(matmul(v, transpose(sf)), transpose(u))
+
+            // this.cf = matvecmul(pse, y)
         }
     }
     determinant(a,b,c,d,e,f,g,h,i){
@@ -418,13 +693,67 @@ class Regression{
 }
 
 
+x = [ 95.00001207,  95.99999531,  96.99997855,  98.00001908,
+    99.00000232,  99.99998556, 101.0000261 , 102.00000934,
+   102.99999258, 103.99997582, 105.00001635, 105.99999959,
+   106.99998283, 108.00002337, 109.00000661, 109.99998985,
+   110.99997309, 112.00001362, 112.99999686, 113.9999801 ,
+   115.00002064, 116.00000388, 116.99998712, 118.00002765,
+   119.00001089, 119.99999413, 120.99997737, 122.00001791,
+   123.00000115, 123.99998439, 125.00002492, 126.00000816,
+   126.9999914 , 127.99997464, 129.00001518, 129.99999842,
+   130.99998166, 132.00002219, 133.00000543, 133.99998867,
+   134.99997191, 136.00001245, 136.99999569, 137.99997893,
+   139.00001947, 140.0000027 , 140.99998594, 142.00002648,
+   143.00000972, 143.99999296, 144.9999762 , 146.00001674,
+   146.99999998, 147.99998322, 149.00002375, 150.00000699,
+   150.99999023, 151.99997347, 153.00001401, 153.99999725,
+   154.99998049, 156.00002102, 157.00000426, 157.9999875 ,
+   159.00002804, 160.00001128, 160.99999452, 161.99997776,
+   163.00001829, 164.00000153, 164.99998477, 166.00002531,
+   167.00000855, 167.99999179, 168.99997503, 170.00001556,
+   170.9999988 , 171.99998204, 173.00002258, 174.00000582,
+   174.99998906, 175.9999723 , 177.00001283, 177.99999607,
+   178.99997931, 180.00001985, 181.00000309, 181.99998633,
+   183.00002686, 184.0000101 , 184.99999334, 185.99997658,
+   187.00001712, 188.00000036, 188.9999836 , 190.00002413,
+   191.00000737, 191.99999061, 192.99997385, 194.00001439,
+   194.99999763, 195.99998087, 197.0000214 , 198.00000464,
+   198.99998788, 200.00002842, 201.00001166, 201.9999949 ,
+   202.99997814, 204.00001867, 205.00000191, 205.99998515,
+   207.00002569, 208.00000893, 208.99999217, 209.99997541,
+   211.00001595, 211.99999918, 212.99998242, 214.00002296]
+
+
+
+y = [-1.518925, -1.519372, -1.519823, -1.520275, -1.520727, -1.521178,
+    -1.521628, -1.522076, -1.522522, -1.522966, -1.523408, -1.523847,
+    -1.524284, -1.524718, -1.525149, -1.525577, -1.526001, -1.526423,
+    -1.526841, -1.527255, -1.527666, -1.528073, -1.528475, -1.528874,
+    -1.529268, -1.529658, -1.530043, -1.530423, -1.530798, -1.531168,
+    -1.531533, -1.531892, -1.532245, -1.532593, -1.532934, -1.533269,
+    -1.533598, -1.533919, -1.534234, -1.534542, -1.534843, -1.535136,
+    -1.535421, -1.535698, -1.535967, -1.536227, -1.536479, -1.536722,
+    -1.536956, -1.53718 , -1.537394, -1.537599, -1.537794, -1.537978,
+    -1.538152, -1.538314, -1.538466, -1.538607, -1.538735, -1.538852,
+    -1.538957, -1.53905 , -1.53913 , -1.539198, -1.539252, -1.539294,
+    -1.539321, -1.539336, -1.539336, -1.539323, -1.539295, -1.539253,
+    -1.539196, -1.539125, -1.539038, -1.538937, -1.53882 , -1.538688,
+    -1.538541, -1.538378, -1.538199, -1.538004, -1.537794, -1.537567,
+    -1.537324, -1.537066, -1.536791, -1.5365  , -1.536193, -1.535869,
+    -1.53553 , -1.535174, -1.534802, -1.534415, -1.534011, -1.533591,
+    -1.533156, -1.532705, -1.532239, -1.531758, -1.531261, -1.530749,
+    -1.530223, -1.529682, -1.529127, -1.528558, -1.527975, -1.527379,
+    -1.52677 , -1.526148, -1.525513, -1.524867, -1.524208, -1.523538,
+    -1.522858, -1.522167, -1.521465, -1.520754, -1.520034, -1.519305]
+
 
 
 // x = [6.230825,6.248279,6.265732]
 // y = [0.312949,0.309886,0.306639472]
 
-// tt = new Regression(x,y,2)
-// console.log(tt.cf)
+tt = new Regression(x,y,2,true)
+console.log(tt.cf)
 // console.log(tt.val(x[0]), tt.val(x[0])-y[0])
 
 
