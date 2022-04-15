b61b26ee41c0a8700f3ff61a5d75c45b18ebad23
Koushik Naskar <koushik.naskar9@gmail.com>
2022-04-15 11:25:13 +0530

    fetch reject on error

--- src/js/version.js
+++ src/js/version.js
@@ -1,16 +1,16 @@
 function versionCheck() {
-    const lastChecked = store.get('lastChecked')
-    const t = new Date();
-    const today= `${t.getDate()}${t.getMonth()}${t.getFullYear()}`
-    if(lastChecked==today) return  // check just once a day
+    // const lastChecked = store.get('lastChecked')
+    // const t = new Date();
+    // const today= `${t.getDate()}${t.getMonth()}${t.getFullYear()}`
+    // if(lastChecked==today) return  // check just once a day
     fetch('https://api.github.com/repos/Koushikphy/Interactive_Data_Editor/releases/latest')
-    .then(response => response.json())
+    .then(response => response.ok ? response.json() : Promise.reject(response))
     .then(data => {
         var body = data.body
         var new_ver = data.tag_name
         var cur_var = `v${require('../../package.json').version}`
         console.log(body, new_ver)
-        store.set('lastChecked',today)
+        // store.set('lastChecked',today)
         if (new_ver != cur_var) {
             var res = dialog.showMessageBoxSync({
                 type: "question",
